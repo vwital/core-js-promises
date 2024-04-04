3f23bb0a5fd4cab2bf37242fdeeb54386ac5c4b2
vwital <wital128@tut.by>
2024-04-04 16:50:56 +0300

    getAllResult solved

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -112,8 +112,11 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+async function getAllResult(promises) {
+  const result = Promise.all(
+    promises.map((promise) => promise.catch(() => null))
+  );
+  return result;
 }
 
 /**
